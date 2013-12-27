56e29a10acd5d847f0afb479f15396a7fda3bd4c
cfalzone <cfalzone@aquent.com>
2013-12-27 12:51:31 -0500

    WEB-172 - Changed things up to use httpclient since post was not working, will support both http and https now I think

--- src/main/java/com/aquent/viewtools/PostTool.java
+++ src/main/java/com/aquent/viewtools/PostTool.java
@@ -5,11 +5,20 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.util.ParameterParser;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.velocity.tools.view.tools.ViewTool;
+import org.omg.DynamicAny.NameValuePairSeqHelper;
 
 import com.dotmarketing.util.Logger;
 
@@ -69,31 +78,24 @@ public class PostTool implements ViewTool {
 	 */
 	public PostToolResponse sendPost(String url, String params) throws Exception {
 		if(inited) {
-			URL obj = new URL(url);
-			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-	 
-			con.setRequestMethod("POST");
-			con.setRequestProperty("User-Agent", USER_AGENT);
-			con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-	 
-			con.setDoOutput(true);
-			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			wr.writeBytes(params);
-			wr.flush();
-			wr.close();
-	 
-			int responseCode = con.getResponseCode();
-	 
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-	 
-			return new PostToolResponse(responseCode, response.toString());
+			
+			PostMethod method = null;
+			
+	        try {
+	            HttpClient client = new HttpClient();
+	            method = new PostMethod(url);
+	            method.setQueryString(params);
+	            client.executeMethod(method);
+	            return new PostToolResponse(method.getStatusCode(), method.getResponseBodyAsString());
+	        } catch (Exception e) {
+	            Logger.error(this, "Exception posting to url: "+url, e);
+	            return new PostToolResponse(888, null);
+	        } finally {
+	            if(method != null) {
+	                method.releaseConnection();
+	            }
+	        }
+	        
 		}
 		
 		return new PostToolResponse(999, null);
@@ -128,23 +130,22 @@ public class PostTool implements ViewTool {
 	 */
 	public PostToolResponse sendGet(String url) throws Exception {
 		if(inited) {
-			URL obj = new URL(url);
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("User-Agent", USER_AGENT);
+			GetMethod method = null;
+	        
+	        try {
+	            HttpClient client = new HttpClient();
+	            method = new GetMethod(url);
+	            client.executeMethod(method);
+	            return new PostToolResponse(method.getStatusCode(), method.getResponseBodyAsString());
+	        } catch (Exception e) {
+	            Logger.error(this, "Exception getting url: "+url, e);
+	            return new PostToolResponse(888, null);
+	        } finally {
+	            if(method != null) {
+	                method.releaseConnection();
+	            }
+	        }
 	 
-			int responseCode = con.getResponseCode();
-	 
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-	 
-			return new PostToolResponse(responseCode, response.toString());
 		}
 		
 		return new PostToolResponse(999, null);
