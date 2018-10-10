f8b9f76f40e2ac5a721bc18613831d932aace089
Wmiranda1 <wmiranda@aquent.com>
2018-10-10 09:52:08 -0400

    removes params and creds from logger

--- src/main/java/com/aquent/viewtools/PostTool.java
+++ src/main/java/com/aquent/viewtools/PostTool.java
@@ -88,9 +88,9 @@ public class PostTool implements ViewTool {
    * @param creds - A credentials object for authenticated requests.
    * @return A PostToolResponse Object
    */
-  public PostToolResponse sendStringData(String url, String data, String method, String contentType, Credentials creds) {
+  public PostToolResponse sendStringData(String url, String data, String method, String contentType) {
     Logger.debug(this, "sendStringData called with url=" + url + ", data=" + data + ", and method=" + method
-        + ", contentType = " + contentType + ", creds=" + UtilMethods.isSet(creds));
+        + ", contentType = " + contentType);
 
     StringEntity entity;
     try {
@@ -229,9 +229,9 @@ public class PostTool implements ViewTool {
    * @param creds - A credentials object for authenticated requests.
    * @return A PostToolResponse Object
    */
-  public PostToolResponse send(String url, Map<String, String> params, String method, String contentType, Credentials creds) {
-    Logger.debug(this, "send(Map) called with url=" + url + ", params=" + params + ", and method=" + method
-        + ", contentType = " + contentType + ", creds=" + UtilMethods.isSet(creds));
+  public PostToolResponse send(String url, Map<String, String method, String contentType) {
+    Logger.debug(this, "send(Map) called with url=" + url + ", and method=" + method
+        + ", contentType = " + contentType);
 
     if (inited) {
       try {
@@ -247,7 +247,7 @@ public class PostTool implements ViewTool {
           appender = "&";
         }
         UrlEncodedFormEntity entity = new UrlEncodedFormEntity(data, Charset.forName("UTF-8"));
-        
+
 
         CloseableHttpClient client;
 
@@ -261,7 +261,7 @@ public class PostTool implements ViewTool {
         } else {
           client = HttpClients.createDefault();
         }
-        
+
         if (method.equalsIgnoreCase(METHOD_POST)) {
           HttpPost m = new HttpPost(url);
           m.setEntity(entity);
@@ -322,9 +322,9 @@ public class PostTool implements ViewTool {
    * @return A PostToolResponse Object
    * @throws Exception from URLDecoder.decode
    */
-  public PostToolResponse send(String url, String params, String method, String contentType, Credentials creds) throws Exception {
-    Logger.debug(this, "send(String) called with url=" + url + ", params=" + params + ", and method=" + method
-        + ", contentType = " + contentType + ", creds=" + UtilMethods.isSet(creds));
+  public PostToolResponse send(String url, String method, String contentType) throws Exception {
+    Logger.debug(this, "send(String) called with url=" + url + ", and method=" + method
+        + ", contentType = " + contentType);
 
     Map<String, String> queryPairs = new LinkedHashMap<String, String>();
     if (params.length() > 0) {
